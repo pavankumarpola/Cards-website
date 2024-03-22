dbb3ef16ce8ae77a8adc5a04d5ba8be18c74036e
pavankumarpola <159992195+pavankumarpola@users.noreply.github.com>
2024-03-22 07:16:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,173 @@
+var productList = [
+    {
+      id: "1",
+      name: "Ayurvedic Herbs",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+      photos: [
+        "https://blog-images-1.pharmeasy.in/2020/10/30173447/shutterstock_647337493-1.jpg",
+        "https://www.rajivdixits.com/cdn/shop/files/patanjali-divya-youvan-churna-100-gm-pack-of-1.jpg?v=1685348449",
+        "https://5.imimg.com/data5/SELLER/Default/2023/1/ZI/AV/MB/7685180/gokshuradi-guggulu-500x500.jpg",
+        
+      ],
+      description:
+        "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [1, 1, 0, 1, 0],
+      isAccessory: false,
+      brand: "Patanzali",
+      price: 299,
+    },
+    {
+      id: "2",
+      name: "Trikut Nutrition",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2060340/2017/9/14/11505387708574-Puma-Men-Black-Solid-Sporty-Jacket-371505387708496-1.jpg",
+      photos: [
+        "https://5.imimg.com/data5/SR/UK/AJ/SELLER-8244985/ayurvedic-medicine.jpg",
+        "https://5.imimg.com/data5/SELLER/Default/2023/1/ZI/AV/MB/7685180/gokshuradi-guggulu-500x500.jpg",
+        "https://www.advanceayurveda.in/wp-content/uploads/2018/10/meshashringi.png",
+      ],
+      description:
+        "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [0, 0, 0, 1, 0],
+      isAccessory: false,
+      brand: "Patanzali",
+      price: 599,
+    },
+    {
+      id: "3",
+      name: "Meshasringi",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2018/9/19/b1373b00-37d4-41a0-a908-5d742a68d3661537343756229-1.jpg",
+      photos: [
+        "https://www.advanceayurveda.in/wp-content/uploads/2018/10/meshashringi.png",
+        "https://5.imimg.com/data5/SELLER/Default/2023/1/ZI/AV/MB/7685180/gokshuradi-guggulu-500x500.jpg",
+        "https://www.rajivdixits.com/cdn/shop/files/patanjali-divya-youvan-churna-100-gm-pack-of-1.jpg?v=1685348449",
+        
+      ],
+      description:
+        "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [1, 1, 1, 1, 1],
+      isAccessory: false,
+      brand: "Patanzali",
+      price: 149,
+    },
+    {
+      id: "4",
+      name: "Panckak Churna",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/3854721/2018/3/12/11520844673915-BARESKIN-Women-Black-Solid-Lightweight-Leather-Jacket-7761520844673718-1.jpg",
+      photos: [
+        "https://drvaidyas.com/cdn/shop/files/Pack-of-2_13.jpg?v=1690545140",
+        "https://www.advanceayurveda.in/wp-content/uploads/2018/10/meshashringi.png",
+        "https://5.imimg.com/data5/SR/UK/AJ/SELLER-8244985/ayurvedic-medicine.jpg",
+        "https://www.rajivdixits.com/cdn/shop/files/patanjali-divya-youvan-churna-100-gm-pack-of-1.jpg?v=1685348449",
+        "https://5.imimg.com/data5/SELLER/Default/2023/1/ZI/AV/MB/7685180/gokshuradi-guggulu-500x500.jpg",
+      ],
+      description:
+        "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [1, 1, 1, 1, 1],
+      isAccessory: false,
+      brand: "Patanzali",
+      price: 299,
+    },
+    {
+      id: "5",
+      name: "Guggul",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/5649908/2018/5/10/6bfe80cd-2f55-42bc-aa7f-e0d6c9e2ac531525936414747-SASSAFRAS-Women-Blue-Solid-Shirt-Dress-3831525936414532-1.jpg",
+      photos: [
+        "https://5.imimg.com/data5/WJ/RU/NJ/SELLER-9818437/guggal-500x500.png",
+        "https://5.imimg.com/data5/SELLER/Default/2023/1/ZI/AV/MB/7685180/gokshuradi-guggulu-500x500.jpg",
+        "https://www.rajivdixits.com/cdn/shop/files/patanjali-divya-youvan-churna-100-gm-pack-of-1.jpg?v=1685348449",
+      ],
+      description:
+        "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [1, 1, 1, 1, 1],
+      isAccessory: false,
+      brand: "Patanzali",
+      price: 129,
+    },
+    {
+      id: "6",
+      name: "Gokshuradi",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803247/2019/5/27/624dac60-4c42-4902-bba1-30a51cc7f43c1558948536543-Apple-Unisex-Smart-Watches-1441558948536225-1.jpg",
+      photos: [
+        "https://5.imimg.com/data5/SELLER/Default/2023/1/ZI/AV/MB/7685180/gokshuradi-guggulu-500x500.jpg",
+        "https://5.imimg.com/data5/WJ/RU/NJ/SELLER-9818437/guggal-500x500.png",
+        "https://5.imimg.com/data5/WJ/RU/NJ/SELLER-9818437/guggal-500x500.png",
+      ],
+      description:
+        "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [1, 1, 1, 1, 1, 1],
+      isAccessory: true,
+      brand: "Patanzali",
+      price: 249,
+    },
+    {
+      id: "7",
+      name: "Laxun Churna",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2019/2/23/dc05b5e5-aa82-493e-bf0b-0c309baf21021550920383101-1.jpg",
+      photos: [
+        "https://gitaayurvedic.com/cdn/shop/products/laxun-churna-100gm-pack-of-2-574385.jpg?v=1682615361",
+        "https://www.rajivdixits.com/cdn/shop/files/patanjali-divya-youvan-churna-100-gm-pack-of-1.jpg?v=1685348449",
+        "https://5.imimg.com/data5/SR/UK/AJ/SELLER-8244985/ayurvedic-medicine.jpg",
+      ],
+      description:
+      "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [1, 1, 1, 1, 1],
+      isAccessory: true,
+      brand: "Patanzali",
+      price: 199,
+    },
+    {
+      id: "8",
+      name: "Yovan Churna",
+      preview:
+        "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/10122083/2019/6/24/e533a691-3908-41b0-8307-1928a37d4ec41561362162650-Samsung-Galaxy-Fit-4801561362161527-1.jpg",
+      photos: [
+        "https://www.rajivdixits.com/cdn/shop/files/patanjali-divya-youvan-churna-100-gm-pack-of-1.jpg?v=1685348449",
+        "https://5.imimg.com/data5/SR/UK/AJ/SELLER-8244985/ayurvedic-medicine.jpg",
+        "https://www.rajivdixits.com/cdn/shop/files/patanjali-divya-youvan-churna-100-gm-pack-of-1.jpg?v=1685348449",
+      ],
+      description:
+      "Ayurveda, a natural system of medicine, originated in India more than 3,000 years ago.",
+      size: [1, 1, 1, 1, 1],
+      isAccessory: true,
+      brand: "Patanzali",
+      price: 59,
+    },
+   
+  ];
+  function getClothingList() {
+    let products = document.getElementById("brand");
+    productList.forEach((list) => {
+      const mainDiv = document.createElement("div");
+      const productNameElement = document.createElement("div");
+      const brandElement = document.createElement("span");
+      const img = document.createElement("img");
+      const priceElement = document.createElement("div");
+  
+      products.append(mainDiv);
+  
+      mainDiv.append(img);
+      mainDiv.append(productNameElement);
+      mainDiv.append(brandElement);
+      mainDiv.append(priceElement);
+  
+      img.setAttribute("src", list.photos[3]);
+      brandElement.innerHTML = list.brand;
+      productNameElement.innerHTML = list.name;
+      priceElement.innerHTML = "Rs " + list.price;
+  
+      mainDiv.setAttribute("class", "productCard");
+      brandElement.setAttribute("class", "brands");
+      productNameElement.setAttribute("class", "prodName");
+      priceElement.setAttribute("class", "price");
+    });
+  }
+  
+  getClothingList();
+  Footer
